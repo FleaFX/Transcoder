ce69ae4d897ccb3a79f868f97bf0aabe78fd6b29
Wouter Naessens <wouter.naessens@sterima.be>
2024-06-09 11:04:12 +0200

    Add Playlist and Channel models

---
+++ src/Transcoder/Model/Channel.cs
@@ -0,0 +1,8 @@
+﻿namespace Transcoder.Model;
+
+/// <summary>
+/// Represents a single channel.
+/// </summary>
+/// <param name="name">The name of the channel.</param>
+/// <param name="uri">The URI where the channel stream can be read.</param>
+public struct Channel(string name, Uri uri);
---
+++ src/Transcoder/Model/Playlist.cs
@@ -0,0 +1,20 @@
+﻿namespace Transcoder.Model;
+
+/// <summary>
+/// Represents a playlist with channels.
+/// </summary>
+/// <param name="channels">The list of channels that makes up the playlist.</param>
+public struct Playlist(Channel[] channels) {
+    public static Playlist Parse(string m3u) {
+        var channels = new List<Channel>();
+        var lines = m3u.Split(Environment.NewLine);
+        for (var i = 0; i < lines.Length; i++) {
+            if (!lines[i].StartsWith("#EXTINF")) continue;
+            var name = lines[i].Split(',')[1].Trim();
+            var uri = new Uri(lines[i + 1]);
+            channels.Add(new Channel(name, uri));
+        }
+
+        return new Playlist([..channels]);
+    }
+}
