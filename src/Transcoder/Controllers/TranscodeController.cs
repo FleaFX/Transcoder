48749231a614ef1acdc3d54b0b2bbdb9f37ef71d
Wouter Naessens <wouter.naessens@sterima.be>
2024-06-10 08:53:33 +0200

    Cleanup

--- src/Transcoder/Controllers/TranscodeController.cs
+++ src/Transcoder/Controllers/TranscodeController.cs
@@ -16,13 +16,7 @@ public class TranscodeController(IConfiguration configuration) : ControllerBase 
     [HttpGet("{channelName}")]
     public IActionResult Get(string channelName) {
         try {
-            // tune into the channel and create the transcoding stream
-            var stream = _playlist[channelName].Transcode();
-
-            // make sure that the stream is disposed when the client disconnects
-            HttpContext.RequestAborted.Register(stream.Dispose);
-
-            return new FileStreamResult(stream, "video/mp2t");
+            return new FileStreamResult(_playlist[channelName].Transcode(), "video/mp2t");
         } catch (IndexOutOfRangeException) {
             return NotFound($"""Channel "{channelName}" not found.""");
         }
